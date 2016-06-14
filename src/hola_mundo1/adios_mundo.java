2f95f976e35a899c88cb4de89088599c7e2fcca2
Usuario <Usuario@TPV2>
2016-06-14 15:45:21 +0100

    quinta modificacion(crear clase adios_mundo en la rama usuairoLINKSYS3789)

---
+++ src/hola_mundo1/adios_mundo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola_mundo1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class adios_mundo {
+    
+      /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("adios mundo");
+        System.out.println("Hoy es dia 20 de marzo");
+    }
+    
+}

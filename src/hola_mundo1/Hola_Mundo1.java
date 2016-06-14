40e220fe22881c919704c7f08b6f2c80f43c36d3
Usuario <Usuario@TPV2>
2016-06-14 14:08:08 +0100

    primera modificacion(subir código sin cambios)

---
+++ src/hola_mundo1/Hola_Mundo1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola_mundo1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Hola_Mundo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+        System.out.println("Hoy es dia 20 de marzo");
+    }
+    
+}
